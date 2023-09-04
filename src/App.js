126a8ad6f3fbdccd8591d99a09779bd8f0081cac
fabian-co <fabian-co@hotmail.com>
2023-09-04 16:55:05 -0500

    estructura base maquetada con flex

--- src/App.js
+++ src/App.js
@@ -3,26 +3,43 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
 import React from 'react';
 
+const defaultTodos = [
+  {text: "Cortar Tomates", completed: true},
+  {text: "Hacer la cama", completed: false},
+  {text: "Desayunar", completed: true},
+  {text: "Trabajar", completed: true},
+]
 
 function App() {
   return (
-    <React.Fragment>
+    <>
+      <main>
+        <div className='nueva_tarea'>
+          <div className='box_nueva_tarea'>
+            <h1>Nueva Tarea</h1>
+            <CreateTodoButton/> 
+          </div>                     
+        </div>
 
-      <TodoCounter completed={16} total={20}/>
-      <TodoSearch />
+        <div className='mis_tareas'>
+          <div className='box_mis_tareas'>
+            <TodoCounter completed={16} total={20}/>
+            <TodoSearch />
 
-      <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-      </TodoList>
-
-      <CreateTodoButton/>
-           
-    </React.Fragment>
+            <TodoList>
+              {defaultTodos.map(todo => (
+                <TodoItem 
+                key={todo.text}
+                text={todo.text}
+                completed = {todo.completed}/>
+              ))}
+            </TodoList>
+          </div>
+        </div>
+      </main>      
+    </>
   );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,8 +1,8 @@
-function TodoItem(){
+function TodoItem({text, completado}){
     return(
       <li>
         <span>V</span>
-        <p>Este es el texto de ToDo</p>
+        <p>{text}</p>
         <span>X</span>
       </li>
     )
